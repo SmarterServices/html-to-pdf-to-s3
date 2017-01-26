43cdf060a15e147fef1b9d6ba1971502c179a278
jordan piepkow <jpiepkow@gmail.com>
2017-01-26 15:32:30 -0500

    added test file back in

--- test.js
+++ test.js
@@ -1,7 +1,29 @@
 var pdfPipe = require("./index");
-var pdf = new pdfPipe({ pdfCrowd: { userName: "", apiKey: "" } });
+var pdf = new pdfPipe({
+  pdfCrowd: {
+    userName: "",
+    apiKey: ""
+  }
+});
+
 pdf
-  .pipeUrl("http://google.com", "testBucket", "test.pdf")
+  .convertUrl(
+  	"http://google.com", 
+  	"TestBucket", 
+  	"testUrl.pdf")
+  .then(res => {
+    console.log(res);
+  })
+  .catch(e => {
+    console.log(e);
+  });
+  //or
+pdf
+  .convertHtml(
+    "<html>regular HTML code</html>",
+    "TestBucket",
+    "testHtml.pdf"
+  )
   .then(res => {
     console.log(res);
   })
